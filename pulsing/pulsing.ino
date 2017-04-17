7bffd45104f66f99e36370334c095c2fb6db59a4
smolghost <eqgrover@gmail.com>
2017-04-17 00:04:22 -0400

    added comments

--- pulsing/pulsing.ino
+++ pulsing/pulsing.ino
@@ -6,30 +6,30 @@
  * Created on: April 5th 2017
 */
 
-#include <Servo.h>
+#include <Servo.h> //include servo library
 
-#define SERVO1_PWM 10
-#define SERVO2_PWM 9
+#define SERVO1_PWM 10 //define first servo to coordinating pin 10 on board
+#define SERVO2_PWM 9 //define second servo to coordinating pin 9 on board
 
-Servo servo_1;
-Servo servo_2;
+Servo servo_1; //establish the var servo_1 as a servo
+Servo servo_2; //establish the var servo_2 as a servo
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  servo_1.attach(SERVO1_PWM);
-  servo_2.attach(SERVO2_PWM);
+  servo_1.attach(SERVO1_PWM); //assign as output
+  servo_2.attach(SERVO2_PWM); //assign as output
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  servo_1.write(0);  
-  delay(1000);
-  servo_1.write(180);
+  servo_1.write(0);  //start servo 1 at 0 degrees
+  delay(1000); 
+  servo_1.write(180); //move servo 1 to 180 degrees
   delay(500);
 
-  servo_2.write(0);
+  servo_2.write(0); //start servo 2 at 0 degrees
   delay(750);
-  servo_2.write(180);
+  servo_2.write(180); //move servo 2 to 180 degrees
   delay(1000);
 }
